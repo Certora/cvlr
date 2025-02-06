751dc73e92c8cd135c70d14b83e8e28eaf51863c
Arie Gurfinkel <arie@certora.com>
2025-02-06 10:27:10 -0500

    chore: cleanup re-export of cvlr-hook

--- cvlr/src/lib.rs
+++ cvlr/src/lib.rs
@@ -29,13 +29,13 @@ pub mod prelude
    
     pub use cvlr_macros::rule as cvlr_rule;
     pub use cvlr_early_panic::early_panic as cvlr_early_panic;
-    pub use cvlr_hook::cvt_hook_start as cvlr_hook_on_entry;
-    pub use cvlr_hook::cvt_hook_end as cvlr_hook_on_exit;
+    pub use cvlr_hook::cvlr_hook_on_entry;
+    pub use cvlr_hook::cvlr_hook_on_exit;
     
     pub use cvlr_macros::rule as rule;
     pub use cvlr_early_panic::early_panic as early_panic;
-    pub use cvlr_hook::cvt_hook_start as hook_on_entry;
-    pub use cvlr_hook::cvt_hook_end as hook_on_exit;
+    pub use cvlr_hook::cvlr_hook_on_entry as hook_on_entry;
+    pub use cvlr_hook::cvlr_hook_on_exit as hook_on_exit;
 }
 
 pub use prelude::*;
