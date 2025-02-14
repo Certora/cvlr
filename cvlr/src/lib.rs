362492e768b9e7f252bcf1a09d5b4258a469a967
Arie Gurfinkel <arie@certora.com>
2025-02-13 22:11:37 -0500

    refactor: re-export is_uint from cvlr

--- cvlr/src/lib.rs
+++ cvlr/src/lib.rs
@@ -12,30 +12,30 @@ pub mod mathint {
 
 pub mod nondet {
     pub use cvlr_nondet::*;
-} 
+}
 
 pub mod log {
     pub use cvlr_log::*;
 }
 
-pub mod prelude
-{
+pub mod prelude {
     pub use super::asserts::*;
 
     pub use super::log::cvlr_log as clog;
     pub use super::nondet::nondet;
     pub use super::nondet::nondet as cvlr_nondet;
 
-   
-    pub use cvlr_macros::rule as cvlr_rule;
     pub use cvlr_early_panic::early_panic as cvlr_early_panic;
     pub use cvlr_hook::cvlr_hook_on_entry;
     pub use cvlr_hook::cvlr_hook_on_exit;
-    
-    pub use cvlr_macros::rule as rule;
-    pub use cvlr_early_panic::early_panic as early_panic;
+    pub use cvlr_macros::rule as cvlr_rule;
+
+    pub use cvlr_early_panic::early_panic;
     pub use cvlr_hook::cvlr_hook_on_entry as hook_on_entry;
     pub use cvlr_hook::cvlr_hook_on_exit as hook_on_exit;
+    pub use cvlr_macros::rule;
 }
 
 pub use prelude::*;
+
+pub use crate::mathint::{is_u128, is_u16, is_u32, is_u64, is_u8};
