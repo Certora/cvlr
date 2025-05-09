1651990e66a08958c0ce6e7f771f72fef2b6fd4b
Arie Gurfinkel <arie@certora.com>
2025-05-09 09:27:23 -0400

    chore: improve macro formatting with rustfmt

--- cvlr-asserts/src/asserts.rs
+++ cvlr-asserts/src/asserts.rs
@@ -1,13 +1,14 @@
+#[rustfmt::skip]
 macro_rules! impl_bin_assert {
     ($name: ident, $pred: tt, $dollar: tt) => {
         #[macro_export]
         macro_rules! $name {
-                                    ($lhs: expr, $rhs: expr $dollar(, $desc: literal)? ) => {{
-                                        cvlr::clog!(stringify!(assert $lhs $pred $rhs));
-                                        cvlr::clog!($lhs, $rhs);
-                                        $crate::cvlr_assert!($lhs $pred $rhs);
-                                    }};
-                                }
+        ($lhs: expr, $rhs: expr $dollar(, $desc: literal)? ) => {{
+            cvlr::clog!(stringify!(assert $lhs $pred $rhs));
+            cvlr::clog!($lhs, $rhs);
+            $crate::cvlr_assert!($lhs $pred $rhs);
+        }};
+    }
         pub use $name;
     };
 }
