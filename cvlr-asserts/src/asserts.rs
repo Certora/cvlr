df031e2740a75a05f7027fc8c2dfaf82944880fb
Arie Gurfinkel <arie@certora.com>
2025-12-08 12:51:05 -0500

    feat: cvlr_assume_X macros
    cvlr_assume_eq, cvlr_assume_ne, ... to match their cvlr_assert_X
    counterparts.

--- cvlr-asserts/src/asserts.rs
+++ cvlr-asserts/src/asserts.rs
@@ -22,3 +22,28 @@ impl_bin_assert!(cvlr_assert_le, <=, $);
 impl_bin_assert!(cvlr_assert_lt, <, $);
 impl_bin_assert!(cvlr_assert_ge, >=, $);
 impl_bin_assert!(cvlr_assert_gt, >, $);
+
+#[rustfmt::skip]
+macro_rules! impl_bin_assume {
+    ($name: ident, $pred: tt, $dollar: tt) => {
+        #[macro_export]
+        macro_rules! $name {
+        ($lhs: expr, $rhs: expr $dollar(, $desc: literal)? ) => {{
+            let lhs = $lhs;
+            let rhs = $rhs;
+            cvlr::clog!(stringify!(assume $lhs $pred $rhs));
+            cvlr::clog!(lhs => stringify!($lhs));
+            cvlr::clog!(rhs => stringify!($rhs));
+            $crate::cvlr_assume!(lhs $pred rhs);
+        }};
+    }
+        pub use $name;
+    };
+}
+
+impl_bin_assume!(cvlr_assume_eq, ==, $);
+impl_bin_assume!(cvlr_assume_ne, !=, $);
+impl_bin_assume!(cvlr_assume_le, <=, $);
+impl_bin_assume!(cvlr_assume_lt, <, $);
+impl_bin_assume!(cvlr_assume_ge, >=, $);
+impl_bin_assume!(cvlr_assume_gt, >, $);
