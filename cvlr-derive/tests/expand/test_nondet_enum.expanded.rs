807d73fde18e76380eaf7fd69f48b3eab3682b92
Arie Gurfinkel <arie@certora.com>
2025-12-08 20:26:11 -0500

    feat: derive(Nondet) for enums

--- cvlr-derive/tests/expand/test_nondet_enum.expanded.rs
+++ cvlr-derive/tests/expand/test_nondet_enum.expanded.rs
@@ -3,4 +3,12 @@ enum MyEnum {
     Variant1,
     Variant2(u64),
 }
+impl ::cvlr::nondet::Nondet for MyEnum {
+    fn nondet() -> MyEnum {
+        match ::cvlr::nondet::nondet::<u64>() {
+            0u64 => MyEnum::Variant1,
+            _ => MyEnum::Variant2(::cvlr::nondet::nondet()),
+        }
+    }
+}
 fn main() {}
