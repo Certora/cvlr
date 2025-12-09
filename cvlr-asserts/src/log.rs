e0b573da1cdad805b5404e6a8f6a30cafc19dd35
Arie Gurfinkel <arie@certora.com>
2025-12-09 14:17:18 -0500

    feat: add no-loc feature to cvlr-asserts
    When no-loc is enabled, location information is replace
    with "<FILE>" and 0. This is useful for testing purposes.

--- cvlr-asserts/src/log.rs
+++ cvlr-asserts/src/log.rs
@@ -13,9 +13,44 @@ pub fn add_loc(file: &str, line: u32) {
     }
 }
 
+#[cfg(not(feature = "no-loc"))]
+#[macro_export]
+macro_rules! cvlr_asserts_core_file {
+    () => {
+        ::core::file!()
+    };
+}
+
+#[cfg(not(feature = "no-loc"))]
+#[macro_export]
+macro_rules! cvlr_asserts_core_line {
+    () => {
+        ::core::line!()
+    };
+}
+
+#[cfg(feature = "no-loc")]
+#[macro_export]
+macro_rules! cvlr_asserts_core_file {
+    () => {
+        "<FILE>"
+    };
+}
+
+#[cfg(feature = "no-loc")]
+#[macro_export]
+macro_rules! cvlr_asserts_core_line {
+    () => {
+        0u32
+    };
+}
+
 #[macro_export]
 macro_rules! add_loc {
     () => {
-        $crate::log::add_loc(core::file!(), core::line!());
+        $crate::log::add_loc(
+            $crate::cvlr_asserts_core_file!(),
+            $crate::cvlr_asserts_core_line!(),
+        );
     };
 }
