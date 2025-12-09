1ae40e73b37ac0e7634edce127d4fad80772e433
Arie Gurfinkel <arie@certora.com>
2025-12-09 16:42:03 -0500

    chore: update tests

--- cvlr-asserts/tests/expand/test_cvlr_assume_eq.expanded.rs
+++ cvlr-asserts/tests/expand/test_cvlr_assume_eq.expanded.rs
@@ -3,7 +3,7 @@ fn main() {
     {
         let lhs = 1;
         let rhs = 1;
-        ::cvlr_log::cvlr_log("stringify! (assume 1 == 1)", &("assume 1 == 1"));
+        ::cvlr_log::cvlr_log("_", &("assume 1 == 1"));
         ::cvlr_log::cvlr_log("1", &(lhs));
         ::cvlr_log::cvlr_log("1", &(rhs));
         ::cvlr_asserts::cvlr_assume_checked(lhs == rhs);
@@ -11,7 +11,7 @@ fn main() {
     {
         let lhs = x;
         let rhs = y;
-        ::cvlr_log::cvlr_log("stringify! (assume x == y)", &("assume x == y"));
+        ::cvlr_log::cvlr_log("_", &("assume x == y"));
         ::cvlr_log::cvlr_log("x", &(lhs));
         ::cvlr_log::cvlr_log("y", &(rhs));
         ::cvlr_asserts::cvlr_assume_checked(lhs == rhs);
@@ -19,7 +19,7 @@ fn main() {
     {
         let lhs = a;
         let rhs = b;
-        ::cvlr_log::cvlr_log("stringify! (assume a == b)", &("assume a == b"));
+        ::cvlr_log::cvlr_log("_", &("assume a == b"));
         ::cvlr_log::cvlr_log("a", &(lhs));
         ::cvlr_log::cvlr_log("b", &(rhs));
         ::cvlr_asserts::cvlr_assume_checked(lhs == rhs);
