aabd6a0792cd628b142c83fdea353161d1367c68
Arie Gurfinkel <arie@certora.com>
2025-12-09 11:31:29 -0500

    feat: guarded assert macro: cvlr_assert_if

--- cvlr-asserts/src/asserts.rs
+++ cvlr-asserts/src/asserts.rs
@@ -47,3 +47,41 @@ impl_bin_assume!(cvlr_assume_le, <=, $);
 impl_bin_assume!(cvlr_assume_lt, <, $);
 impl_bin_assume!(cvlr_assume_ge, >=, $);
 impl_bin_assume!(cvlr_assume_gt, >, $);
+
+#[macro_export]
+macro_rules! cvlr_assert_if {
+    ($guard: expr, $cond: expr) => {
+        if $guard {
+            $crate::cvlr_assert!($cond);
+        }
+    };
+}
+
+#[rustfmt::skip]
+macro_rules! impl_bin_assert_if {
+    ($name: ident, $pred: tt, $dollar: tt) => {
+        #[macro_export]
+        macro_rules! $name {
+        ($guard: expr,$lhs: expr, $rhs: expr $dollar(, $desc: literal)? ) => {{
+            let guard = $guard;
+            cvlr::clog!(stringify!(assert $guard ==> $lhs $pred $rhs));
+            cvlr::clog!(guard => stringify!($guard));
+            if guard {
+                let lhs = $lhs;
+                let rhs = $rhs;
+                cvlr::clog!(lhs => stringify!($lhs));
+                cvlr::clog!(rhs => stringify!($rhs));
+                $crate::cvlr_assert!(lhs $pred rhs);
+            }
+        }};
+    }
+        pub use $name;
+    };
+}
+
+impl_bin_assert_if!(cvlr_assert_eq_if, ==, $);
+impl_bin_assert_if!(cvlr_assert_ne_if, !=, $);
+impl_bin_assert_if!(cvlr_assert_le_if, <=, $);
+impl_bin_assert_if!(cvlr_assert_lt_if, <, $);
+impl_bin_assert_if!(cvlr_assert_ge_if, >=, $);
+impl_bin_assert_if!(cvlr_assert_gt_if, >, $);
