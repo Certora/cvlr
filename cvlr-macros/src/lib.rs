b3682895bd219a588a5cd0cc64df59fe6161a14e
Arie Gurfinkel <arie@certora.com>
2025-12-10 09:26:02 -0500

    feat: initial version of cvlr_assert_that macro

--- cvlr-macros/src/lib.rs
+++ cvlr-macros/src/lib.rs
@@ -2,6 +2,7 @@ use proc_macro::TokenStream;
 use quote::ToTokens;
 use syn::{parse_macro_input, parse_quote, ItemFn};
 
+mod assert_that;
 mod mock;
 /// Mark a method as a CVT rule
 ///
@@ -36,4 +37,9 @@ pub fn rule(_attr: TokenStream, item: TokenStream) -> TokenStream {
 #[proc_macro_attribute]
 pub fn mock_fn(attr: TokenStream, item: TokenStream) -> TokenStream {
     mock::mock_fn_impl(attr, item)
+}
+
+#[proc_macro]
+pub fn cvlr_assert_that(input: TokenStream) -> TokenStream {
+    assert_that::assert_that_impl(input)
 }
