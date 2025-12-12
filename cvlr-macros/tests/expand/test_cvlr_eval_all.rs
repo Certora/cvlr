f374e1a010efb961eac0fe2b9fa72ba13406a8b1
Arie Gurfinkel <arie@certora.com>
2025-12-12 14:09:52 -0500

    feat: cvlr_eval_all macro
    The macro is similar to cvlr_eval_that!, but instead of evaluating a
    single condition, it evaluates multiple conditions as a boolean
    expression.
    
    This is useful for testing the DSL of cvlr predicates.

---
+++ cvlr-macros/tests/expand/test_cvlr_eval_all.rs
@@ -0,0 +1,36 @@
+use cvlr_macros::cvlr_eval_all;
+
+pub fn test_eval_all() {
+    let x = 5;
+    let y = 10;
+    let c = true;
+    let flag = true;
+    let a = 1;
+    let b = 2;
+
+    // Multiple unguarded expressions
+    let _result1 = cvlr_eval_all!(x > 0, y < 20, x < y);
+
+    // Mixed guarded and unguarded
+    let _result2 = cvlr_eval_all!(x > 0, if c { x < y });
+
+    // Using semicolons
+    let _result3 = cvlr_eval_all!(x > 0; y < 20; if c { x < y });
+
+    // Mixed separators
+    let _result4 = cvlr_eval_all!(x > 0, y < 20; if c { x < y });
+
+    // Boolean expressions
+    let _result5 = cvlr_eval_all!(flag, x > 0 && y < 20);
+
+    // Guarded boolean expressions
+    let _result6 = cvlr_eval_all!(if flag { x > 0 }, if c { y < 20 });
+
+    // Complex expressions
+    let _result7 = cvlr_eval_all!(x + 1 > 0, y * 2 < 30, if a > 0 { b < 10 });
+}
+
+pub fn main() {
+    test_eval_all();
+}
+
